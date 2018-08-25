ae4ebf3655fba57b58475f0335e531c5a09b402c
Saad Mirza <saadmirza009@gmail.com>
2018-08-25 20:30:44 +0500

    Starting

---
+++ movies/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Movies(models.Model):
+    title = models.CharField(max_length=60)
+    genreId= models.IntegerField()
+    numberInStock = models.IntegerField()
+    dailyRentalRate = models.FloatField()
