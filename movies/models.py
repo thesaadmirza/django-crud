43fedd57161ee24440c6e35332916eb7280c3ac6
Saad Mirza <saadmirza009@gmail.com>
2018-08-27 20:24:11 +0500

    updated model with genre foreign key

--- movies/models.py
+++ movies/models.py
@@ -2,6 +2,9 @@ from django.db import models
 # Create your models here.
 class Movies(models.Model):
     title = models.CharField(max_length=60)
-    genreId= models.IntegerField()
+    genres= models.name = models.ForeignKeyField(Genres, on_delete=models.CASCADE)
     numberInStock = models.IntegerField()
     dailyRentalRate = models.FloatField()
+
+class Genres(models.Model):
+    name = CharField(max_length=60)
